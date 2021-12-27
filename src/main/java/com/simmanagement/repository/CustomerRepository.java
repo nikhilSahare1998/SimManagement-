01b8fea685227daa51a97e4b186fe7aacbbd9be5
nikhilSahare1998 <93664405+nikhilSahare1998@users.noreply.github.com>
2021-12-27 08:17:23 +0530

    Update CustomerRepository.java

--- src/main/java/com/simmanagement/repository/CustomerRepository.java
+++ src/main/java/com/simmanagement/repository/CustomerRepository.java
@@ -4,7 +4,6 @@ import com.simmanagement.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
@@ -12,7 +11,7 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
     @Query(value = "SELECT * FROM  customer WHERE  DATE_ADD(date_of_birth,INTERVAL YEAR(CURDATE())-YEAR(date_of_birth)+ IF(DAYOFYEAR(CURDATE()) > DAYOFYEAR(date_of_birth),1,0)YEAR)BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 7 DAY)", nativeQuery = true)
     List<Customer> getCustomer();
 
-
+    
     @Query(value = "SELECT * FROM  customer WHERE  DATE_ADD(date_of_birth,INTERVAL YEAR(CURDATE())-YEAR(date_of_birth)+ IF(DAYOFYEAR(CURDATE()) > DAYOFYEAR(date_of_birth),1,0)YEAR)BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 1 DAY)", nativeQuery = true)
     List<Customer> getCustomersListOneDayBeforeBirthday();
 }
